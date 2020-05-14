27930c53a015fd59cab4712041bbaa5af4d20a7e
Samartha <41548444+yunghog@users.noreply.github.com>
2020-05-14 19:17:50 +0530

    footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-12 text-center">
-        <p>Designed & Developed | <a href="https://www.instagram.com/samartha__">Samartha</a></p>
+        <p>Designed & Developed | <a href="https://www.instagram.com/samartha__" target="_blank">Samartha</a></p>
       </div>
     </div>
   </div>
