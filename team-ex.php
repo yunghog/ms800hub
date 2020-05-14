254d7bd4c4cedc78a8a371fd537d627b1e984361
Samartha <41548444+yunghog@users.noreply.github.com>
2020-05-14 19:01:35 +0530

    team

---
+++ team-ex.php
@@ -0,0 +1,166 @@
+<?php  ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+		<link href="https://fonts.googleapis.com/css?family=Monoton&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Lexend+Deca&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Club MS8</title>
+  </head>
+	<body background="assets/img/bg.jpg">
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+  <a class="navbar-brand" href="#"><img class="logo" src="assets/img/logo.png" alt="">		</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto" >
+      <li class="nav-item ">
+        <a class="nav-link" href="index.html">HOME <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="event.html">EVENTS</a>
+      </li>
+			<li class="nav-item">
+        <a class="nav-link" href="#">GALLERY</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="blog.php">BLOG</a>
+      </li>
+			<li class="nav-item">
+        <a class="nav-link" href="register.html">REGISTER</a>
+      </li>
+			<!-- <li class="nav-item">
+        <a class="nav-link" href="about">ABOUT</a>
+      </li> -->
+      <li class="nav-item dropdown active" >
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          ABOUT
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color:#353a40;">
+        <a class="nav-link" href="aboutus.html">ABOUT US</a>
+        <a class="nav-link" href="team.html">TEAM</a>
+          <div class="dropdown-divider"></div>
+        </div>
+      </li>
+      <!-- <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" >Disabled</a>
+      </li> -->
+    </ul>
+
+  </div>
+</nav>
+		</header>
+<br>
+  <div class="content">
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+        <h1 class="big-text">TEAM</h1>
+        <br>
+        <h2 class="tag">Founders of the club</h2>
+      </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-4">
+          <div class="card border-dark" style="border:none;border-radius: 10px;" >
+            <img class="card-img-top" src="assets/img/admin1.jpg" alt="Card image cap">
+            <div class="card-body" style="background-color:black;">
+              <h5 class="card-title para">Samartha</h5>
+              <span class="team-sub">Founder</span>
+              <p class="card-text"><span class="para">20yo developer and music producer based in Sagar. Passionate about cars.
+                Highly influenced by Dad's car workshop 'Aditya Automobiles'.<br>
+                <a href="https://www.instagram.com/samartha__/"><i class="fa fa-instagram">  </i></a>
+                <a href="https://www.youtube.com/c/YUNGHOGbeats"><i class="fa fa-youtube">  </i></a>
+                <a href="https://soundcloud.com/user-649397724"><i class="fa fa-soundcloud">  </i></a>
+              </span></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card border-dark" style="border:none;border-radius: 10px;" >
+            <img class="card-img-top" src="assets/img/admin2.jpg" alt="Card image cap">
+            <div class="card-body" style="background-color:black;">
+              <h5 class="card-title para">Sadeep</h5>
+              <span class="team-sub">Co-Founder</span>
+              <p class="card-text"><span class="para">
+                Student and Illustrator from Kerala. Influenced by Keralians' love towards vehicles and modification culture. Based in Sagar.
+                <br>
+                <a href="https://www.instagram.com/sady_walker/"><i class="fa fa-instagram">  </i></a>
+                <a href="https://www.youtube.com/c/YUNGHOGbeats"><i class="fa fa-youtube">  </i></a>
+              </span></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-2"></div>
+      </div>
+        <br>
+        <div class="row">
+          <div class="col-12">
+            <h2 class="tag">OUR SPONSERS</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-2"></div>
+          <div class="col-sm-8">
+            <div class="card border-dark" style="border:none;border-radius: 10px;" >
+              <img class="card-img-top" src="assets/img/sponser1.jpg" alt="Card image cap">
+              <div class="card-body" style="background-color:black;">
+                <h5 class="card-title para">Aditya Automobiles</h5>
+                <span class="team-sub">The authorised Maruti Suzuki service station</span>
+                <p class="card-text"><span class="para">
+                The well know car workshop of Sagar located at Jog Road. All sort of services like oil change,engine tuning,body shop works,
+                 repairing, painting, tinkering, washing..... for MS cars is done here.
+                  Friendly staffs and highly experienced technicians take good care of your car.
+                  <br>
+                  <a href="https://www.instagram.com/repair.race/"><i class="fa fa-instagram">  </i></a>
+                  <a href="https://www.youtube.com/c/YUNGHOGbeats"><i class="fa fa-youtube">  </i></a>
+                </span></p>
+                <span class="team-sub">Locate us</span>
+                <div class="card-img-top" style="background-color:black;" >
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4098.442319930923!2d75.01825792896285!3d14.170489764234565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x18a683fe2f2cf9e!2sAditya%20Automobiles!5e0!3m2!1sen!2sin!4v1579870937881!5m2!1sen!2sin" width="100%" height="300px" frameborder="3px" style="border:0;" allowfullscreen=""></iframe>
+                </div>
+
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-2"></div>
+        </div>
+        </div>
+      </div>
+      <br><br>
+      <footer>
+    <div class="footer">
+      <p>© 2020 Copyright: ms800hub.rf.gd | by <a href="https://www.instagram.com/samartha__">Samartha</p></a>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
