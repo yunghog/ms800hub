b70339aa65657d501ce039938efa526a59944607
Samartha <41548444+yunghog@users.noreply.github.com>
2020-05-17 18:05:24 +0530

    footer

---
+++ community/footer.php
@@ -0,0 +1,9 @@
+<footer id="foo">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 text-center">
+        <p>Designed & Developed | <a href="https://www.instagram.com/samartha__" target="_blank">Samartha</a></p>
+      </div>
+    </div>
+  </div>
+</footer>
