4e900567e928145ddd7f52ef4b0ac37aa618c6cf
Samartha <41548444+yunghog@users.noreply.github.com>
2020-05-17 23:10:04 +0530

    blog post handler

--- assets/php/blog_post.php
+++ assets/php/blog_post.php
@@ -1,39 +1,50 @@
 <?php
-include 'db_connect.php';
 session_start();
-$target_dir = "../img/blogimg/";
-$target_file = $target_dir . basename($_FILES["bimg"]["name"]);
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {?>
-      <script type="text/javascript">
-        alert("Sorry, only JPG, JPEG and PNG  files are allowed");
-        window.location.href='../../create-blog.php';
-      </script>
-<?php
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "<script>
-            alert('Sorry, file already exists');
-            window.location.href='../../create-blog.php';
-            </script>";
-}
-else{
-move_uploaded_file($_FILES["bimg"]["tmp_name"], $target_file);
-$orginal_target_dir="assets/img/blogimg/";
-$orginal_target_file= $orginal_target_dir . basename($_FILES["bimg"]["name"]);
-$title=$_POST['title'];
-$para=$_POST['para'];
-$user_id=$_SESSION['user_id'];
-$d='y,m,d';
-$date=date($d);
-echo mysqli_error($conn);
-$query="INSERT into blog(title,para,user_id,image,tdate) values('$title','$para','$user_id','$orginal_target_file','$date')";
-$exec=mysqli_query($conn,$query);
-}
+include 'db_connect.php';
+   if(isset($_FILES['image'])){
+     $user_id=$_SESSION['user_id'];
+     $title=$_POST['title'];
+     $body=$_POST['body'];
+     $tag=$_POST['tag'];
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size =$_FILES['image']['size'];
+      $file_tmp =$_FILES['image']['tmp_name'];
+      $file_type=$_FILES['image']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      $today=date('d-m-y');
+      $time=time();
+      $file_name=$time.'.'.$file_ext;
+      $extensions= array("jpeg","jpg","png");
+
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      }
+
+      if($file_size > 2097152){
+         $errors[]='File size must be excately 2 MB';
+      }
+
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"../img/blog/"."blog_image_".$user_id.$file_name);
+         $target="blog_image_".$user_id.$file_name;
+         //$query="INSERT INTO `gallery` (`image`) VALUES ('$target')";
+         $query="INSERT INTO `blog`(`image`, `author_id`, `title`, `body`, `date`, `tag`) VALUES ('$target','$user_id','$title','$body','$today','$tag')";
+         $exec=mysqli_query($conn,$query);
+         ?>
+         <script type="text/javascript">
+           alert('Uploaded successfully');
+         </script>
+         <?php
+      }else{
+         ?>
+          <script type="text/javascript">
+            alert('<?php $error; ?>');
+          </script>
+         <?php
+      }
+   }
 ?>
 <script type="text/javascript">
-  alert('Blog Posted');
-  window.location.href='../../create-blog.php';
+  window.location.href='../../admin/blog.php';
 </script>
