c1bf658953a615f75e791bf0879ffc0d343767f3
Samartha <41548444+yunghog@users.noreply.github.com>
2020-05-17 18:05:37 +0530

    gallery delete

---
+++ assets/php/gallery_delete.php
@@ -0,0 +1,11 @@
+<?php
+include 'db_connect.php';
+$id=$_POST['gallery_id'];
+$query = "UPDATE gallery set active=0 where id='$id'";
+$exec = mysqli_query($conn,$query);
+//echo "id = $id";
+ ?>
+ <script type="text/javascript">
+   alert("Post deleted");
+   window.location.href = '../../signin.php';
+</script>
