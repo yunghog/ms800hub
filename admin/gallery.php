a9cfca3ec9317ded31c7383ebe17c4c36cf6b377
Samartha <41548444+yunghog@users.noreply.github.com>
2020-05-18 22:51:49 +0530

    gallery

--- admin/gallery.php
+++ admin/gallery.php
@@ -29,6 +29,10 @@ if (isset($_SESSION['user_id'])){
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">
+        <!-- DataTable -->
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.21/datatables.min.css"/>
+        <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.21/datatables.min.js"></script>
+
       </head>
     	<body background="../assets/img/bg.jpg">
         <?php include 'header.php'; ?>
@@ -43,8 +47,8 @@ if (isset($_SESSION['user_id'])){
             </div>
             <br>
             <div class="row">
-              <div class="col-md-12 table-column">
-                <table class="table table-striped bg-black " id="gallery-table">
+              <div class="col-md-12 table-column text-primary">
+                <table class="table table-dark " id="gallery-table">
                   <thead>
                     <tr>
                       <th>ID</th>
@@ -127,12 +131,20 @@ if (isset($_SESSION['user_id'])){
               </div>
           </div>
         </div>
+        <br><br><br>
         <?php include 'footer.php'; ?>
         <script type="text/javascript">
           document.getElementById('index').classList.add('active2');
           // document.getElementById('foo').style.position="static";
         </script>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script type="text/javascript">
+        $(document).ready(function() {
+    $('table').DataTable( {
+        "responsive":   true,
+    } );
+} );
+        </script>
+        <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
       </body>
