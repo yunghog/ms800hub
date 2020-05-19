9ad5199f1605288e8430d4023bc56a1456377ef8
Samartha <41548444+yunghog@users.noreply.github.com>
2020-05-19 17:02:34 +0530

    blog

--- blog.php
+++ blog.php
@@ -98,7 +98,7 @@ include 'assets/php/db_connect.php';
                               echo $blog_rows['date'][$i];
                             } ?></small>
                             <br>
-                              <p><?php echo $blog_rows['body']; ?></p>
+                              <p class="text-justify"><?php echo $blog_rows['body']; ?></p>
                           </div>
                         </div>
                       </div>
@@ -141,7 +141,7 @@ include 'assets/php/db_connect.php';
                   window.open('https://www.facebook.com/sharer/sharer.php?u=' + link, 'facebook-popup', 'height=350,width=600');
                 }
                 if(arg=='whatsapp'){
-                  link='https://web.whatsapp.com/send?text=%20' + link;
+                  link='https://api.whatsapp.com/send?text=%20' + link;
                   window.open(link);
                 }
                 if(arg=='link'){
@@ -182,7 +182,6 @@ include 'assets/php/db_connect.php';
         window.location.href="blog.php";
       }
     </script>
-
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
